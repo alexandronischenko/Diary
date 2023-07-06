3ac43569cfc1f6536f880296df8d9eb2c38cd252
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2023-07-06 18:49:24 +0300

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Alexandr Onischenko on 06.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
